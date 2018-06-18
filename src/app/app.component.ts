a10baf25ee6889e8ae27fd0371410f0394d8c29a
sejaldhaval <sejaldhaval@gmail.com>
2018-06-18 15:41:00 -0500

    added top navbar
    top navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dhaynu Tech Solutions...';
+  title = 'Dhyanu Tech Solutions...';
 }
