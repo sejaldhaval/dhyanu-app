b5b6ba7660ef63980d532f384b95468821880176
sejaldhaval <sejaldhaval@gmail.com>
2018-07-26 15:09:45 -0500

    changes in UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Dhyanu Software Solutions...';
+  
+  ngOnInit() {
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 100) {
+        $("#main-navbar").addClass("main-navar-scrolled");
+      }
+      else {
+        $("#main-navbar").removeClass("main-navar-scrolled");
+      }
+    });
+  }
 }
